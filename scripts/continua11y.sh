4cac5496dc65318754ffb1d9514fa8abe677e089
stvnrlly <stevenmreilly+github@gmail.com>
2015-06-25 08:56:48 -0400

    add commit message to continua11y

--- scripts/continua11y.sh
+++ scripts/continua11y.sh
@@ -15,18 +15,22 @@ then
     # if your site generates a sitemap, set this to true to use it instead of spidering
     USE_SITEMAP=false
     # the location for the locally-running version of continua11y
+    # for local development, set the protocol for cURL to http, as well
     CONTINUA11Y="localhost:3000"
 else
+    # we're on travis, so install the tools needed
     npm install -g pa11y
     npm install -g pa11y-reporter-1.0-json
     npm install -g json
 fi
 
+TRAVIS_COMMIT_MSG="$(git log --format=%B --no-merges -n 1)"
+
 # set up the JSON file for full results to send
-echo '{"repository":"'$TRAVIS_REPO_SLUG'", "branch": "'$TRAVIS_BRANCH'","commit":"'$TRAVIS_COMMIT'","data":{}}' | json > results.json
+echo '{"repository":"'$TRAVIS_REPO_SLUG'", "branch": "'$TRAVIS_BRANCH'","commit":"'$TRAVIS_COMMIT'","commit_message":"'$TRAVIS_COMMIT_MSG'","data":{}}' | json > results.json
 
 function runtest () {
-    pa11y -r 1.0-json $a > pa11y.json
+    pa11y -r json $a > pa11y.json
     
     # single apostrophes ruin JSON parsing, so remove them
     sed "s/'//g" pa11y.json
@@ -52,14 +56,13 @@ else
 fi
 
 # iterate through URLs and run runtest on each
-cat sites.txt | while read a; do runtest $a; done
+cat sites | while read a; do runtest $a; done
 
 # close down the server
 eval $KILL_SCRIPT
 
 # send the results on to continua11y
 curl -X POST http://${CONTINUA11Y}/incoming -H "Content-Type: application/json" -d @results.json
-cat results.json
 
 # clean up
 rm results.json pa11y.json sites.txt
